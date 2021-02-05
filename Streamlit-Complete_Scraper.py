06d8350ff1c141eaf9ab070dd42a44a4d775118c
John Pravin <68058163+JohnPravin97@users.noreply.github.com>
2021-02-05 10:42:05 +0530

    Update Streamlit-Complete_Scraper.py

--- Streamlit-Complete_Scraper.py
+++ Streamlit-Complete_Scraper.py
@@ -204,7 +204,7 @@ def spelling_checker(x):
     crt_inp=''
     search = '+'.join(x.split())
     url='https://www.google.com/search?q='+ search
-    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    driver = webdriver.Chrome("/app/streamlit--georgie-the-scraper-tool/Webdriver/chromedriver.exe")
     driver.get(url)
     html = driver.page_source
     soup = BeautifulSoup(html,'lxml')
